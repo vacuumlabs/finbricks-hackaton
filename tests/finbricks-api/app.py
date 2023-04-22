3cb2ad453418e7d93572ccd85ad4e5d485bf2e43
michalzan <michal.zan@vacuumlabs.com>
2023-04-22 11:56:08 +0200

    jwt signature gen

--- tests/finbricks-api/app.py
+++ tests/finbricks-api/app.py
@@ -1,9 +1,9 @@
-import json
-
-import jwt, requests
 from base64 import urlsafe_b64encode
 
-private_key = open("/home/bohuss/projects/finbricks/finbricks_private.pem", "r").read()
+import jwt
+import requests
+
+private_key = open("/Users/michalzan/.ssh/finbricks_private.pem", "r").read()
 
 b_private_key = b"${private_key}"
 
@@ -12,14 +12,6 @@ b_private_key = b"${private_key}"
 
 jwsHeader = b'{"typ":"JWT", "alg":"RS256", "kid":"cb7f0803-143b-4f10-8cac-0657fa17b109"}'
 
-authBody = {
-    "merchantId": "ec9e2133-520f-4ca0-9e12-f167339d232e",
-    "clientId": "hackathon2023_vl",
-    "provider": "MOCK_COBS",
-    "scope": "AISP",
-    "callbackUrl": "https://www.vacuumlabs.cz/callback"
-}
-
 payload = {"uri":"/account/list?merchantId=ec9e2133-520f-4ca0-9e12-f167339d232e&clientId=hackathon2023_vl&paymentProvider=MOCK_COBS","body":""}
 
 encodedHeader = urlsafe_b64encode(jwsHeader)
@@ -27,10 +19,11 @@ encodedHeader = urlsafe_b64encode(jwsHeader)
 #
 # data = f"${encodedHeader}.${encodedPayload}"
 
-encoded_jwt = jwt.encode(payload, b_private_key, algorithm="HS256")
+encoded_jwt = jwt.encode(payload, b_private_key, algorithm="HS256", headers={"typ":"JWT", "alg":"HS256", "kid":"cb7f0803-143b-4f10-8cac-0657fa17b109"})
+# encoded_Header = jwt.encode(jwsHeader)
 
-jwsSignature = f"${encodedHeader}..${encoded_jwt}"
+jwsSignature = f"{encodedHeader}..{encoded_jwt}"
 
-response = requests.get("https://api.sandbox.finbricks.com/account/list?merchantId=ec9e2133-520f-4ca0-9e12-f167339d232e&clientId=hackathon2023_vl&paymentProvider=MOCK_COBS", headers={'JWS-Signature': jwsSignature})
-
+response = requests.get("https://api.sandbox.finbricks.com/account/list?merchantId=ec9e2133-520f-4ca0-9e12-f167339d232e&clientId=hackathon2023_vl&paymentProvider=MOCK_COBS", headers={'JWS-Signature': encoded_jwt})
+print(jwsSignature)
 print(response.json())
