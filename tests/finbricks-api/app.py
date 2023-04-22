19d157ab433efbdc36b6f21462ac175c05819a74
michalzan <michal.zan@vacuumlabs.com>
2023-04-22 13:27:07 +0200

    jwt signature gen

--- tests/finbricks-api/app.py
+++ tests/finbricks-api/app.py
@@ -1,22 +1,51 @@
 import base64
+import json
 from base64 import urlsafe_b64encode
 
 import jwt
 import requests
+
 # from jose import jws
 
 private_key = open("/Users/michalzan/.ssh/finbricks_private.pem", "r").read()
 
-jwsHeader = '{"typ":"JWT", "alg":"RS256", "kid":"cb7f0803-143b-4f10-8cac-0657fa17b109"}'
-encodedHeader = urlsafe_b64encode(jwsHeader.encode('utf-8')).decode('utf-8')
+kid = "ec9e2133-520f-4ca0-9e12-f167339d232e"
+clientId = "hackathon2023_vl"
+base_url = 'https://api.sandbox.finbricks.com'
+accounts_endpoint = f'/account/list?merchantId={kid}&clientId={clientId}&paymentProvider=MOCK_COBS'
+auth_endpoint = '/auth/authenticate'
+auth_body = {
+    "merchantId": kid,
+    "clientId": clientId,
+    "provider": "MOCK_COBS",
+    "scope": "AISP",
+    "callbackUrl": "https://mujweb.cz/redirect-from-fbx.html"
+}
 
-payload = {"uri":"/account/list?merchantId=ec9e2133-520f-4ca0-9e12-f167339d232e&clientId=hackathon2023_vl&paymentProvider=MOCK_COBS","body":""}
 
-encoded_jwt = jwt.encode(payload, private_key, algorithm="RS256", headers={"kid":"cb7f0803-143b-4f10-8cac-0657fa17b109"})
+def get_signature(endpoint, body):
+    jws_header = {"typ": "JWT", "alg": "RS256", "kid": kid}
+    payload = {
+        "uri": endpoint,
+        "body": body}
 
-jwsSignature = f"{encodedHeader}..{encoded_jwt}"
+    # print(payload)
+    encoded_jwt = jwt.encode(payload, private_key, algorithm="RS256", headers=jws_header)
+    split = encoded_jwt.split('.')
+    return f'{split[0]}..{split[2]}'
 
-response = requests.get("https://api.sandbox.finbricks.com/account/list?merchantId=ec9e2133-520f-4ca0-9e12-f167339d232e&clientId=hackathon2023_vl&paymentProvider=MOCK_COBS", headers={'JWT-Signature': encoded_jwt})
 
-print(jwsSignature)
+auth_response = requests.post(
+    f'{base_url}{auth_endpoint}',
+    json=auth_body,
+    headers={'JWS-Signature': get_signature(auth_endpoint, json.dumps(auth_body))}
+)
+
+response = requests.get(
+    f'{base_url}{accounts_endpoint}',
+    headers={'JWS-Signature': get_signature(accounts_endpoint, "")})
+
+
+# print(auth_body)
+print(auth_response.json())
 print(response.json())
